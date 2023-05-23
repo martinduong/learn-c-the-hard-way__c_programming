8d3c437b0cd0876da40e9539fb8456c4dff1af01
Martin Duong <marty.san@gmail.com>
2023-05-23 20:53:22 +0000

    finished ex10

--- Ex10/ex10.c
+++ Ex10/ex10.c
@@ -3,13 +3,13 @@
 int main(int argc, char *argv[])
 {
   if(argc != 2){
-    printf("ERROR: You need one argument")
+    printf("ERROR: You need one argument");
       //This is how to abort a program
       return 1;
   }
 
   int i = 0;
-  for(i = 0; argv[1][i] != '0'; i++){
+  for(i = 0; argv[1][i] != '\0'; i++){
     char letter = argv[1][i];
 
     switch (letter) {
@@ -17,6 +17,35 @@ int main(int argc, char *argv[])
     case 'A':
       printf("%d: 'A'\n", i);
       break;
+    case 'e':
+    case 'E':
+      printf("%d: 'E'\n", i);
+      break;
+    case 'i':
+    case 'I':
+      printf("%d: 'I'\n", i);
+      break;
+    case 'o':
+    case 'O':
+      printf("%d: 'O'\n", i);
+      break;
+    case 'u':
+    case 'U':
+      printf("%d: 'O'\n", i);
+      break;
+    case 'y':
+    case 'Y':
+      printf("%d: 'Y'\n", i);
+      if (i > 2){
+        //it's only sometimes Y
+        printf("%d: 'Y'\n",i);
+      }
+      break;
+
+    default:
+      printf("%d: %c is not a vowel.\n", i, letter);
     }
   }
+
+  return 0;
 }
